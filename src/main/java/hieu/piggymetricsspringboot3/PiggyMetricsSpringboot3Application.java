d0632f76c482c77b6c231ba49cc558286ad70a5a
hieunm2 <hieunm2@vnext.vn>
2025-04-24 09:52:00 +0700

    init commit

---
+++ src/main/java/hieu/piggymetricsspringboot3/PiggyMetricsSpringboot3Application.java
@@ -0,0 +1,13 @@
+package hieu.piggymetricsspringboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiggyMetricsSpringboot3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiggyMetricsSpringboot3Application.class, args);
+    }
+
+}
---
+++ src/test/java/hieu/piggymetricsspringboot3/PiggyMetricsSpringboot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package hieu.piggymetricsspringboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiggyMetricsSpringboot3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
